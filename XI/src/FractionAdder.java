37e31d8e361a13e149fbf7d23cc0d5e05131d423
Satvik Saha <sahasatvik@gmail.com>
2018-01-15 23:16:27 +0530

    Added error handling in FractionAdder

--- XI/src/FractionAdder.java
+++ XI/src/FractionAdder.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class FractionAdder {
 	public static void main (String[] args) {
 		Scanner inp = new Scanner(System.in);
-		
-		/* Get the two fractions from user input */
-		System.out.print("Enter the numerator and denominator [integer integer] of the first fraction  : ");
-		Fraction a = new Fraction(inp.nextInt(), inp.nextInt());
-		System.out.print("Enter the numerator and denominator [integer integer] of the second fraction : ");
-		Fraction b = new Fraction(inp.nextInt(), inp.nextInt());
-		
-		/* Calculate and display the sum of the fractions.
-		   Here, we take advantage of the toString() method defined for Fractions */
-		Fraction sum = Fraction.addFractions(a, b);
-		System.out.printf("%n(%s) + (%s) = (%s) %n", a, b, sum);
+		try {
+			/* Get the two fractions from user input */
+			System.out.print("Enter the numerator and denominator [integer integer] of the first fraction  : ");
+			Fraction a = new Fraction(inp.nextInt(), inp.nextInt());
+			System.out.print("Enter the numerator and denominator [integer integer] of the second fraction : ");
+			Fraction b = new Fraction(inp.nextInt(), inp.nextInt());
+			
+			/* Calculate and display the sum of the fractions.
+			   Here, we take advantage of the toString() method defined for Fractions */
+			Fraction sum = Fraction.addFractions(a, b);
+			System.out.printf("%n(%s) + (%s) = (%s) %n", a, b, sum);
+		} catch (ArithmeticException e) {
+			System.out.println("Invalid fraction - division by zero!");
+		}
 	}
 }

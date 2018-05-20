766166b5243561f03c9e8576810364269eec1294
Satvik Saha <sahasatvik@gmail.com>
2018-05-20 17:14:55 +0530

    Added Towers of Hanoi Solver

---
+++ ISC/src/TowersOfHanoi.java
@@ -0,0 +1,14 @@
+class TowersOfHanoi {
+	public static void main (String[] args) {
+		int disks = Integer.parseInt(args[0]);
+		solveHanoi(disks, "A", "B", "C");
+	}
+
+	public static void solveHanoi (int disk, String source, String destination, String spare) {
+		if (disk == 0)
+			return;
+		solveHanoi(disk - 1, source, spare, destination);
+		System.out.printf("(%d) : %s -> %s%n", disk, source, destination);
+		solveHanoi(disk - 1, spare, destination, source);
+	}
+}
